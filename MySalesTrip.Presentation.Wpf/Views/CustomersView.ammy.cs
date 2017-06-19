e2824c159d2cd0cf417b184bc249b68ee95c59e8
Jason Regnier <jason.regnier4@gmail.com>
2017-06-18 23:18:18 -0400

    Converting to AmmyUI
    Switch over the Customers and Trips Xaml files to AmmyUI files

---
+++ MySalesTrip.Presentation.Wpf/Views/CustomersView.ammy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySalesTrip.Presentation.Wpf.Views
+{
+    public partial class CustomersView
+    {
+        public CustomersView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MySalesTrip.Presentation.Wpf/Views/TripsView.ammy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySalesTrip.Presentation.Wpf.Views
+{
+    public partial class TripsView
+    {
+        public TripsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
